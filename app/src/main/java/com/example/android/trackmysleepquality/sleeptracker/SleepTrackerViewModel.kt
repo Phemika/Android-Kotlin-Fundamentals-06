a3fc4c89c09489d478ea528b1a8969fdf524c104
a <pamika.si@rmuti.ac.th>
2020-08-05 10:55:06 +0700

    06.3 Use LiveData to control button states(3. Task: Add navigation)step2

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -20,6 +20,7 @@ import android.app.Application
 import android.provider.SyncStateContract.Helpers.insert
 import android.provider.SyncStateContract.Helpers.update
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
@@ -83,6 +84,8 @@ class SleepTrackerViewModel(
             val oldNight = tonight.value ?: return@launch
             oldNight.endTimeMilli = System.currentTimeMillis()
             update(oldNight)
+            _navigateToSleepQuality.value = oldNight
+
         }
     }
     private suspend fun update(night: SleepNight) {
@@ -102,5 +105,13 @@ class SleepTrackerViewModel(
             database.clear()
         }
     }
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
 }
+
 
